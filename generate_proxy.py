c045e79c9b9f1ec349cad9113cce7e77a4717a50
Proxy98 <125541610+AliDev-98@users.noreply.github.com>
2023-03-26 07:45:24 -0700

    Update generate_proxy.py

--- generate_proxy.py
+++ generate_proxy.py
@@ -1,17 +1,17 @@
 import random
 
 def generate_proxy():
-    # توليد البروكسي
+    # generate proxy
     proxy = f"{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}:{random.randint(1000,9999)}"
     return proxy
 
 def save_proxies(num_proxies):
-    # افتح الملف للكتابة
+    # Please open the file for writing
     with open("proxies.txt", "w") as file:
-        # توليد عدد num_proxies من البروكسيات وحفظها في الملف
+        # Generate num_proxies proxies and save them in a file
         for i in range(num_proxies):
             proxy = generate_proxy()
             file.write(proxy + "\n")
 
-# توليد 10 بروكسيات وحفظها في ملف باسم proxies.txt
+# Generate 10 proxies and save them in a file named proxies.txt
 save_proxies(1000000)
